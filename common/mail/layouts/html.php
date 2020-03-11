8ad5b4acfb2d4e5c22bfba474843305e7eadcdd7
jamalWeb <jamal.absalimov@yandex.ru>
2020-03-12 01:34:12 +0300

    Code style php-csfix

--- common/mail/layouts/html.php
+++ common/mail/layouts/html.php
@@ -9,12 +9,13 @@ use yii\web\View;
  * @var $message MessageInterface the message being composed
  * @var $content string main view render result
  */
+$charset = Yii::$app->charset;
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ru">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset= <?= Yii::$app->charset?> ">
+    <meta http-equiv="Content-Type" content=<?= "text/html; charset= {$charset} " ?>>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
