37c03a2aa6812d73a94d01554a26ba244cf2e722
jamalWeb <jamal.absalimov@yandex.ru>
2020-03-21 22:09:04 +0300

    set autorized user

--- backend/controllers/Base/BaseController.php
+++ backend/controllers/Base/BaseController.php
@@ -2,10 +2,24 @@
 
 namespace backend\controllers\Base;
 
+use common\models\user\User;
+use Yii;
 use yii\web\Controller;
 
+/**
+ * @property-read User $authorizedUser
+ */
 class BaseController extends Controller
 {
+    public $authorizedUser;
+
+    public function __construct($id, $module, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+
+        $this->authorizedUser = Yii::$app->user->identity;
+    }
+
     public function getViewPath(): string
     {
         return $this->module->getBasePath(
