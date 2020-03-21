13918777bba1422d0e444d913ed1fa68f0c37e7b
jamalWeb <jamal.absalimov@yandex.ru>
2020-03-21 16:07:12 +0300

    Задал структуру dto объекта

--- backend/Entity/Services/User/Dto/UserLoginDto.php
+++ backend/Entity/Services/User/Dto/UserLoginDto.php
@@ -4,7 +4,14 @@
 namespace backend\Entity\Services\User\Dto;
 
 
+use common\models\user\User;
+
 class UserLoginDto
 {
+    /** @var User */
+    public $user;
+
+    /** @var bool */
+    public $rememberMe;
 
 }
