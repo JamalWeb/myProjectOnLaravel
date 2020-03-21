dfcb4ef76367489ea1a06222827132df8a7ff04f
jamalWeb <jamal.absalimov@yandex.ru>
2020-03-21 14:32:27 +0300

    Interface работы с бд

---
+++ backend/Entity/Services/User/Repository/UserRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace backend\Entity\Services\User\Repository;
+
+
+interface UserRepositoryInterface
+{
+    public function save();
+
+    public function get(string $field, $value);
+}
