e8989482b235507edf4ecb869d0182044744627a
jamalWeb <jamal.absalimov@yandex.ru>
2020-04-04 21:47:49 +0300

    Реализую удаление пользователей с подтверждением

---
+++ backend/controllers/User/Action/ActionView.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace backend\controllers\User\Action;
+
+
+use backend\controllers\Base\BaseAction;
+use backend\controllers\User\UserController;
+
+/**
+ * @property-read UserController $controller
+ */
+class ActionView extends BaseAction
+{
+    public function run(int $id): string
+    {
+        $user = $this->controller->service->findOne('id', $id);
+
+        $this->controller->registerMeta("Пользователь: {$user->username}", '', '');
+
+        return $this->controller->render(
+            'view',
+            [
+                'user' => $user,
+            ]
+        );
+    }
+
+}
---
+++ backend/controllers/User/View/view.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @var View $this
+ * @var ActiveDataProvider $dataProvider
+ * @var User $user
+ */
+
+use common\models\user\User;
+use yii\bootstrap4\Html;
+use yii\web\View;
+use yii\widgets\DetailView;
+
+?>
+
+<h4>
+    <?= $this->title ?> |
+    <?= Html::a(
+        '<ion-icon name="trash-outline" size="small" style="color:red"></ion-icon>',
+        [
+            'user/delete',
+            'id' => $user->id,
+        ],
+        [
+            'id' => 'deleteItem',
+        ]
+    ) ?>
+</h4>
+<?= DetailView::widget(
+    [
+        'model'      => $user,
+        'attributes' => [
+            [
+                'label'     => '№',
+                'attribute' => 'id',
+            ],
+            [
+                'attribute' => 'profile.id',
+                'label'     => 'Аватар',
+                'value'     => $user->profile->avatar,
+            ],
+            'username',
+            'profile.first_name',
+            'profile.last_name',
+            'profile.patronymic',
+            'profile.phone_number',
+            'profile.address',
+            'profile.short_lang',
+            'profile.about:ntext',
+            'type.name',
+            'email',
+            'logged_in_ip',
+            'logged_in_at',
+            'logout_in_at',
+            'profile.gender.name',
+            'created_ip',
+            'is_banned',
+            'banned_at',
+            'status.name',
+            'created_at',
+            'updated_at',
+        ],
+    ]
+) ?>
